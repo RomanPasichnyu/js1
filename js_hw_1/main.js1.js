7ba9aa487c6fa50459008b50ef4d4f4d38e9ee55
Роман <romjkea@gmail.com>
2023-11-20 23:17:18 +0200

    hw1_done

---
+++ js_hw_1/main.js1.js
@@ -0,0 +1,46 @@
+let n1='hello';
+console.log(n1);
+let n2='owu';
+console.log(n2);
+let n3='com';
+console.log(n3);
+let n4='ua';
+console.log(n4);
+let n5=1;
+console.log(n5);
+let n6=10;
+console.log(n6);
+let n7 =-999;
+console.log(n7);
+let n8=123;
+console.log(n8);
+let n9=3.14;
+console.log(n9);
+let n10=2.7;
+console.log(n10);
+let n11=16;
+console.log(n11);
+let n12=true;
+console.log(n12);
+let n13=false;
+console.log(n13);
+
+let firstName='Paichnyk';
+let middleName='Roman';
+let lastName='Yurievych';
+console.log(firstName+middleName+lastName);
+
+let a=100;
+console.log(typeof a);
+console.log(a);
+
+
+let b='100';
+console.log(typeof b);
+console.log(b);
+
+let c=true;
+console.log(typeof c);
+console.log(c);
+
+
