2f88027d4a81932b2f56ddefb4855a5f82075696
Роман <romjkea@gmail.com>
2023-12-26 21:09:13 +0200

    hw6_done

--- js_hw_9/mainjs9.js
+++ js_hw_9/mainjs9.js
@@ -50,7 +50,8 @@ let coursesAndDurationArray2 = [
     {title: 'FullStack', monthDuration: 7},
     {title: 'Frontend', monthDuration: 4}
 ];
-// За допомоги скріпта для кожного елементу масиву зробити <div class='item'> ,  в якому буде <h1 class='heading'>  з title  елементу,
+// За допомоги скріпта для кожного елементу масиву зробити <div class='item'> ,
+// в якому буде <h1 class='heading'>  з title  елементу,
 // та <p class='description'> з monthDuration елементу.
 //     Завдання робити через цикли.
 for (const coursElem of coursesAndDurationArray2) {
